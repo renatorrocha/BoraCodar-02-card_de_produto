096895686f32398a1e7434293e9988c8feb679fd
renatorrodrigues <renatorrodrigues2002@gmail.com>
2023-09-26 10:36:28 -0300

    fix: card-view screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import product from "public/product.png";
 
 export default function Home() {
   return (
-    <main>
-      <figure>
+    <main className="flex h-screen w-full justify-center">
+      <figure className="flex items-center justify-center">
         <Image src={product} alt="" />
       </figure>
-      <div>
+      <div className="flex flex-col justify-center">
         <p className=" font-lato text-xs font-light uppercase">código: 42404</p>
-        <p className="font-crimsonPro">Sofá Margot II - Rosé</p>
-        <p className="font-lato">R$ 4.000 </p>
-        <button className="uppercase">adicionar à cesta</button>
+        <p className="font-crimsonPro text-3xl my-2">Sofá Margot II - Rosé</p>
+        <p className="font-lato text-base font-normal">R$ 4.000 </p>
+        <button className="items-center rounded-3xl border-2 border-light_purple px-4 py-2 uppercase mt-2">
+          adicionar à cesta
+        </button>
       </div>
     </main>
   );
